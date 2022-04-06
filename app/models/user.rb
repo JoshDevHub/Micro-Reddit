f630a6b9d43f766fd688f9b5fff456d5807b5910
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-06 14:35:23 -0600

    feat: add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
